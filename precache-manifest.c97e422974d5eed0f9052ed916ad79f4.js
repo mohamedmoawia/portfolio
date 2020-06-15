614e16d77ae31481288daba0c52c73467ff3a08f
mohamedmoawia <mo3wia94@gmail.com>
2020-06-15 21:51:17 +0200

    Updates

---
+++ precache-manifest.c97e422974d5eed0f9052ed916ad79f4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4eeb7b0ead998ca572ffc5aff4c2375",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "406f45ad7dd8da976a92",
+    "url": "/portfolio/static/css/main.f3bc71f0.chunk.css"
+  },
+  {
+    "revision": "5ebb17b4a7d5bc8deaae",
+    "url": "/portfolio/static/js/2.27265e4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.27265e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406f45ad7dd8da976a92",
+    "url": "/portfolio/static/js/main.1df3a226.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "7ed5dfd9fa6c564cfb730c9f31e4dd41",
+    "url": "/portfolio/static/media/Shortly.7ed5dfd9.jpeg"
+  },
+  {
+    "revision": "bf0d4074f5cb34c46f9cdd828f7b1a18",
+    "url": "/portfolio/static/media/calculator.bf0d4074.jpeg"
+  },
+  {
+    "revision": "382e1e18ad14cb51ae5a480a77e28179",
+    "url": "/portfolio/static/media/drum-pad.382e1e18.jpeg"
+  },
+  {
+    "revision": "2b1037da342580e5e360a0460bc49679",
+    "url": "/portfolio/static/media/pomodro-clock.2b1037da.jpeg"
+  },
+  {
+    "revision": "16908c3d6732436753bb7c3133575c7d",
+    "url": "/portfolio/static/media/quote-machine.16908c3d.jpeg"
+  },
+  {
+    "revision": "e82104c789461f67ac7d24db7fb3e9a4",
+    "url": "/portfolio/static/media/video-search.e82104c7.PNG"
+  }
+]);
